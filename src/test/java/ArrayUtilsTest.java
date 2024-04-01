7b5496ab6963d44018af2582ebe0e727af87a73e
Audrey-H <1712802830@qq.com>
2024-03-31 21:58:30 -0400

    Add new test cases

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,9 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testTrail() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, -2, 1, 4}));
+    }
 }
