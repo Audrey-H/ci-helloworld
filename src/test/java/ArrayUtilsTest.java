171177a94bce27582233cb293f28507df4e4cadc
Audrey-H <1712802830@qq.com>
2024-04-02 16:38:06 -0400

    Remove all test cases

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayUtilsTest {
 
+    /*
     @Test
     public void testFindLastNull() {
         // Do not execute fault
@@ -46,7 +47,7 @@ public class ArrayUtilsTest {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
 
-    /*
+
     @Test
     public void testNoMatch() {
         int[] array = {1, 2, 3, 4, 5};
